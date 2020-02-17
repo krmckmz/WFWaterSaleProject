7c6db824b720ac9c869c5b6cff7b2692af0e9e18
Kerem ÇÖKMEZ <58333792+krmckmz@users.noreply.github.com>
2020-02-17 13:10:13 +0300

    Add files via upload

---
+++ Model/CustomerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFAWater.Entity;
+
+namespace WFAWater.Model
+{
+    public class CustomerModel
+    {
+        public int ID { get; set; }
+        public string adi { get; set; }
+        public string soyadi { get; set; }
+        public string telefon { get; set; }
+        public string adres { get; set; }
+        public bool aktifMi { get; set; }
+    }
+}
---
+++ Model/OrderModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFAWater.Entity;
+
+namespace WFAWater.Model
+{
+    public class OrderModel
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+        public int Durum { get; set; }
+        public decimal Tutar { get; set; }
+        public DateTime KayitTarihi { get; set; }
+        public CustomerModel Customer = new CustomerModel();
+        public bool isDeleted { get; set; }
+    }
+}
